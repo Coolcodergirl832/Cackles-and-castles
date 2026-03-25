131cf65cad7884e1adaf72dcb073f8252672854d
Tessa <rhcb20@gmail.com>
2026-03-25 17:55:08 -0400

    Add files via upload

--- CAC 3 game bundle/main_menue.py
+++ CAC 3 game bundle/main_menue.py
@@ -5,3 +5,6 @@ print("what installment of the series do you want to play?")
 game = input("1, 2, or 3? ")
 if game == "1":
     the_first_game()
+if game == "2":
+    print("the second game is currently in development, check back later!")
+if game == "3":    print("the third game is currently in development, check back later!")
